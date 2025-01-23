af8ebf3283fea1872c8cfe14ea0cf53e484f2fc5
volkde <ilyaprofit@gmail.com>
2025-01-23 20:26:43 +0100

    Add type HW06

--- src/homeworks/Homework06/types.ts
+++ src/homeworks/Homework06/types.ts
@@ -14,10 +14,10 @@ let mixed: (number|string)[] = [1, "Alice", 2, "Bob"];
 
 let colors: (number| string| boolean)[] = [123, "red", true, "blue"];
 
-const add:(a: number, b: number):number = (a, b) => {
+const add = (a: number, b: number):number => {
   return a - b;
 };
 
-const logMessage:() = (message:string): void => {
+const logMessage = (message:string): void => {
   console.log(message);
 };
